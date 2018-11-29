de8eeaeb3df77c8271dc800ea718ca06bb4cede3
Matthieu BERTRAND <xplode-ccm@hotmail.fr>
2018-11-29 11:51:44 +0100

    It works !

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import os
 import logging
 # logging.warn(os.environ["DUMMY"])
 
-from flask import Flask
+from flask import Flask, request
 from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,7 +13,7 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 from models import Product
-from schemas import products_schema
+from schemas import products_schema, product_schema
 
 @app.route('/hello')
 def hello():
@@ -23,3 +23,39 @@ def hello():
 def products():
     products = db.session.query(Product).all() # SQLAlchemy request => 'SELECT * FROM products'
     return products_schema.jsonify(products)
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    content = request.get_json("name")
+    product = Product()
+    product.name = content["name"]
+    db.session.add(product)
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+@app.route('/products/<int:id>')
+def get_product(id):
+    product = db.session.query(Product).get(id)
+    if not product:
+        return 'Product does not exist', 404
+    return product_schema.jsonify(product)
+
+@app.route('/products/<int:id>', methods=['PATCH'])
+def update_product(id):
+    content = request.get_json("name")
+    product = db.session.query(Product).get(id)
+    if not product:
+        return 'Product does not exist', 404
+    product.name = content["name"]
+    db.session.add(product)
+    db.session.commit()
+    return product_schema.jsonify(product)
+
+@app.route('/products/<int:id>', methods=['DELETE'])
+def delete_product(id):
+    product = db.session.query(Product).get(id)
+    if not product:
+        return 'Product does not exist', 404
+    db.session.delete(product)
+    db.session.commit()
+    return f'Product with id {id} deleted', 200
